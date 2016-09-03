bc5079ac3cf1ee3eed99c4c561f8cb077cbda475
ankush1990 <ank_ankush@yahoo.co.in>
2016-09-03 18:54:28 +0530

    201609031854

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -23,8 +23,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 	var notificationOpenedCallback = function(jsonData) {
 		//console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
 		
-		alert('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
-		
+		alert(jsonData.data.foo);
 		var data = { origin:'indore', quantity:37, thickness:37, width:37, length:37, currency:37, price:37, title:37};
 		$state.go('app.offers-detail',data);
 	};
